10f1ce0d73a999be69e88622b8a37801eb8c807c
Alabi12 <wolo22001@gmail.com>
2022-11-08 16:22:26 +0000

    updated the question three

--- index.test.js
+++ index.test.js
@@ -12,20 +12,55 @@ test("characters", () => {
 test("function should return reverse string", () => {
   expect(reverseString("hello")).toBe("olleh");
 });
-describe("calculator", () => {
+// ex 3
+describe("calculator add", () => {
   test("add method should return num1 plus num2", () => {
     expect(calculator.add(3, 4)).toBe(7);
   });
-  test("subtract method should return num2 minus num1", () => {
-    expect(calculator.subtract(4, 3)).toBe(1);
+  test("add method should return num1 plus num2", () => {
+    expect(calculator.add(4, 2)).toBe(6);
   });
+  test("add method should return num1 plus num2", () => {
+    expect(calculator.add(1, 4)).toBe(5);
+  });
+});
+
+describe("calculator subtract", () => {
+  test("subtract method should return num2 minus num1", () => {
+    expect(calculator.subtract(20, 15)).toBe(5);
+  });
+  test("subtract method should return num2 minus num1", () => {
+    expect(calculator.subtract(40, 30)).toBe(10);
+  });
+  test("subtract method should return num2 minus num1", () => {
+    expect(calculator.subtract(13, 3)).toBe(10);
+  });
+});
+
+describe("calculator divide", () => {
   test("divide method should return num1 divide num2", () => {
     expect(calculator.divide(4, 2)).toBe(2);
   });
+  test("divide method should return num1 divide num2", () => {
+    expect(calculator.divide(40, 20)).toBe(2);
+  });
+  test("divide method should return num1 divide num2", () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+  });
+});
+
+describe("calculator multiply", () => {
   test("multiply method should return num1 multiply num2", () => {
     expect(calculator.multiply(3, 4)).toBe(12);
   });
+  test("multiply method should return num1 multiply num2", () => {
+    expect(calculator.multiply(1, 4)).toBe(4);
+  });
+  test("multiply method should return num1 multiply num2", () => {
+    expect(calculator.multiply(4, 2)).toBe(8);
+  });
 });
+// ex 4
 test("character method should return the first char to be uppercase", () => {
   expect(capitalize("hello")).toBe("Hello");
 });
